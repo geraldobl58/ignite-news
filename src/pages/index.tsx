28bc592ca9efc3de687c2015090abbee6654521f
Geraldo Luiz <geraldobl58@gmail.com>
2021-04-07 19:57:02 -0300

    feat: Subscribe Button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -14,7 +16,9 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publications <br/>
-          <span>for $9.90 month</span></p>
+            <span>for $9.90 month</span>
+            <SubscribeButton />
+          </p>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
